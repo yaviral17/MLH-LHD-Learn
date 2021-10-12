43e3ed94077a25060aa04e8c32fc685aa2ff8467
yaviral17 <58760825+yaviral17@users.noreply.github.com>
2021-10-12 16:12:31 +0530

    Add files via upload

---
+++ Movie-recommender/Movie Recommender.py
@@ -0,0 +1,29 @@
+import random
+
+def horror():
+    with open("HORROR.txt","r") as f:
+        line=f.readlines()
+        chh=random.choice(line)
+        return chh
+
+def action():
+    with open("Action.txt","r") as f:
+        line=f.readlines()
+        chh=random.choice(line)
+        return chh
+
+def romantic():
+    with open("romantic.txt","r") as f:
+        line=f.readlines()
+        chh=random.choice(line)
+        return chh
+
+
+ch=random.choice(["Action","Horror","Romantic"])
+
+if ch == "Action":
+    print("Today you should : ",action())
+elif ch=="Romantic":
+    print("Today you should watch : ",romantic())
+else :
+    print("Today you should watch : ",horror())
